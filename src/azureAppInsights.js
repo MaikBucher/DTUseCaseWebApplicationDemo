dd7807aa5aaa3c29cba90319a35a83df44b64f16
Maik Bucher <maik.bucher@codecentric.de>
2024-11-25 11:35:46 +0100

    Test

--- src/azureAppInsights.js
+++ src/azureAppInsights.js
@@ -1,11 +1,11 @@
 import { ApplicationInsights } from '@microsoft/applicationinsights-web';
+console.log(process.env.VUE_APP_INSTRUMENTATION_KEY);
 
 const appInsights = new ApplicationInsights({
     config: {
         instrumentationKey : process.env.VUE_APP_INSTRUMENTATION_KEY
     }
 });
-
 appInsights.loadAppInsights();
 
 export default appInsights;
